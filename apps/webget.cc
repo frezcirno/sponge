0ab330ce942342b2d4e1ff8739cdde8d723633eb
frezcirno <2277861660@qq.com>
2022-02-10 00:15:36 +0800

    webget: change to FullStackSocket

--- apps/webget.cc
+++ apps/webget.cc
@@ -7,7 +7,7 @@
 using namespace std;
 
 void get_URL(const string &host, const string &path) {
-    CS144TCPSocket sock{};
+    FullStackSocket sock{};
     sock.connect({host, "http"});
     sock.write("GET " + path + " HTTP/1.1\r\n"
                "Host: " + host + "\r\n"
